59979453bfe620c9dd602485d120eff6397df69d
KareM Ch <chkaren29@gmail.com>
2022-03-30 16:59:05 -0400

    detalles de menu

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const boton_nav = document.querySelector(".bar");
 const lista_menu = document.querySelector(".lista-menu");
-
+const lista_items = document.querySelectorAll(".nav-link-item");
 boton_nav.addEventListener("click", () => {
  lista_menu.classList.toggle("bar-visible");
 });
+
+const accionMenu = () => {
+    lista_menu.classList.toggle("bar-visible");
+};
+
+lista_items.forEach(element => element.addEventListener("click", accionMenu));
